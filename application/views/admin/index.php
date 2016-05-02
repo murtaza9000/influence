e3b61d8377ef3e0106bbbaa267df2007b64c6ba4
Mustafa Hanif <mustafa.hanif@screeninteraction.com>
2016-05-02 10:57:03 +0400

    no message

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -304,7 +304,7 @@ desired effect
                     <div class="row">
                         <div class="col-md-3 col-sm-6 col-xs-12">
                             <div class="info-box">
-                                <span class="info-box-icon bg-purple"><i class="ion ion-ios-people-outline"></i></span>
+                                <span class="info-box-icon bg-purple"><i class="ion ion-social-usd-outline"></i></span>
 
                                 <div class="info-box-content">
                                     <span class="info-box-text">Fetch Past Analytics History</span>
